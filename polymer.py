b0ac9de76181373fba6f938ee7506c4791d69426
AmelieLaurens <66320985+AmelieLaurens@users.noreply.github.com>
2020-07-23 12:44:05 +0200

    Update polymer.py

--- polymer.py
+++ polymer.py
@@ -11,7 +11,7 @@ from deck import Deck
 class Polymer:
 
     # Initializer Attributes
-    def __init__(self, deck = Deck("deck.yaml")):
+    def __init__(self, deck):
         self.name = deck.doc['Polymers']['Name']
         self.density = float(deck.doc['Polymers']['Density'])
         self.viscosity = float(deck.doc['Polymers']['Viscosity'])
