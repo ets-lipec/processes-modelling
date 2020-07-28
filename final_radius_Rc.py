dce15053858d30d3472d1a75ba0c0b0a0a0d85ed
AmelieLaurens <66320985+AmelieLaurens@users.noreply.github.com>
2020-07-28 19:26:45 +0200

    Update final_radius_Rc.py

--- final_radius_Rc.py
+++ final_radius_Rc.py
@@ -9,105 +9,79 @@ Source : A simple model for nanofiber formation by rotary jet-spinning
 by mellado et al.
 
 Goal : Prediction of the final radius for various collector distances.
+Select the machine and the polymer for which we want to run the code and ajust values
+in deck.yaml file.
 
-Select the machine and the polymer for which we want to run the code.
-
-Inputs : Concerning the polymer : the surface tension, the density, the viscosity
-         Concerning the machine : the reservoir radius, the orifice radius,
-                                  the angular viscosity of the spinneret.
+Polymer parameters : the density, the viscosity, the surface tension
+Machine parameters  : the reservoir radius, the orifice radius,
+                      the angular viscosity of the spinneret.
+Discretisation number
 
 All data are in SI units.
-
 """
 
-from models_RJS import *
+from deck import Deck
+from machine import RJSMachine
+from polymer import Polymer
+from modelling import RJSModel
 import numpy
 import matplotlib.pyplot as plt
 
-discretisation = 20
+deck = Deck("deck.yaml")
+machine = RJSMachine(deck)
+polymer = Polymer(deck)
+model = RJSModel(polymer, machine)
+
+discretisation = int(deck.doc['Discretisation'])
 # The higher the discretisation number is, the finer the discretisation will be,
 # there will be more points on the graphic.
 
+# Reach machine parameters
+name_machine = machine.name
+s0 = machine.reservoir_radius
+omega = machine.omega
+orifice_radius = machine.orifice_radius
 
-# Choose one machine and one polymer
-# Machines
-# Super Floss Maxx
-# s0 = 0.06985
-# orifice_radius = 0.001512
-# omega = 57.5
+# Reach polymer parameters
+name_polymer = polymer.name
+rho = polymer.density
+mu = polymer.viscosity
+surface_tension = polymer.surface_tension
 
-# CANDY-V001
-s0 = 0.0635
-orifice_radius = 0.000267
-omega = 40.
-
-
-# Polymers
-# PP
-# surface_tension = 0.0436
-# rho = 900.
-# mu = 0.63
-
-# PLA
-surface_tension = 0.0248
-rho = 1250.
-mu = 0.113
-
-# If we want the user to enter its own values
-# s0 = float(input("Enter the radius of the reservoir in m : "))
-# surface_tension =float(input("Enter the surface tension in kg/s^2 : "))
-# orifice_radius = float(input("Enter the radius of the orifice in m : "))
-# rho = float(input("Enter the density of the polymer in kg/m^3 : "))
-# mu = float(input("Enter the viscosity of the polymer in Pa.s : "))
-# omega = float(input("Enter the angular velocity in rounds per second : "))
-# discretisation = int(input("Enter an int for the mesh's thinness : "))
-
-
-omega_th = critical_rotational_velocity_threshold(surface_tension,
+omega_th = model.critical_rotational_velocity_threshold(surface_tension,
                                                   orifice_radius, s0, rho)
-initial_velocity = Initial_velocity(omega_th, s0)
+initial_velocity = model.Initial_velocity(omega_th, s0)
 
-nu = kinematic_viscosity(mu, rho)
+nu = model.kinematic_viscosity(mu, rho)
 
 Rc = numpy.linspace(0.1, 0.5, discretisation)
 
-
-final_radius = []
+Final_radius = []
 for k in range(discretisation):
-    final_radius.append(final_radius_approx(orifice_radius, initial_velocity,
+    Final_radius.append(model.final_radius(orifice_radius, initial_velocity,
                                             nu, Rc[k], omega))
-final_radius = numpy.array(final_radius)
+Final_radius = numpy.array(Final_radius)
 
 fig = plt.figure()
 axes = fig.add_subplot(1, 1, 1)
 
 for i in range(discretisation):
-    axes.plot(Rc[i], final_radius[i], 'ro')
+    axes.plot(Rc[i], Final_radius[i], 'ro')
 axes.grid()
 axes.set_xlabel("Collector distance (m)", fontsize=16)
 axes.set_ylabel("Final radius (m)", fontsize=16)
-
-# Choose a title
-# axes.set_title("Final radius as a function of the collector distance ", fontsize=16)
-# axes.set_title("Super Floss Maxx  / PP ", fontsize=16)
-# axes.set_title("Super Floss Maxx  / PLA", fontsize=16, y=1.)
-# axes.set_title("CANDY-V001  / PP", fontsize=16, y=1.)
-axes.set_title("CANDY-V001  / PLA", fontsize=16, y=1.)
+axes.set_title(" %s / %s " % (name_machine, name_polymer), fontsize=16, y=1.)
 
 # Plot a zoomed graphic on the small radius below 0.00002 m
 fig2 = plt.figure()
 axes = fig2.add_subplot(1, 1, 1)
 
 for i in range(discretisation-1):
-    if final_radius[i] <= 0.00002:
-        axes.plot(Rc[i], final_radius[i], 'bo')
+    if Final_radius[i] <= 0.00002:
+        axes.plot(Rc[i], Final_radius[i], 'bo')
 axes.grid()
 axes.set_xlabel("Collector distance (m)", fontsize=16)
 axes.set_ylabel("Final radius  (m)", fontsize=16)
+axes.set_title("ZOOM %s / %s " % (name_machine, name_polymer), fontsize=16, y=1.05)
 
-# Choose one title
-# axes.set_title("ZOOM Final radius as a function of the collector distance ", fontsize=16)
-# axes.set_title("ZOOM Super Floss Maxx  / PP ", fontsize=16)
-# axes.set_title("ZOOM Super Floss Maxx  / PLA", fontsize=16, y=1.)
-# axes.set_title("ZOOM CANDY-V001  / PP", fontsize=16, y=1.)
-axes.set_title("ZOOM CANDY-V001  / PLA", fontsize=16, y=1.)
+plt.show()
