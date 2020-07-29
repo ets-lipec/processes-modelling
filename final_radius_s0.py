eaed6d2c053f04a658673e55da935049b584388a
AmelieLaurens <66320985+AmelieLaurens@users.noreply.github.com>
2020-07-29 20:05:22 +0200

    changes

--- final_radius_s0.py
+++ final_radius_s0.py
@@ -10,13 +10,11 @@ by mellado et al.
 
 Goal : Prediction of the final radius for various reservoir radius.
 
-Select the machine and the polymer for which we want to run the code and ajust values
-in deck.yaml file.
+Select the machine and the polymer for which we want to run the code.
 
-Polymer parameters : the density, the viscosity, the surface tension
-Machine parameters  : the collector radius, the orifice radius,
-                      the angular viscosity of the spinneret.
-Discretisation number
+Inputs : Concerning the polymer : the surface tension, the density, the viscosity
+         Concerning the machine : the collector radius, the orifice radius,
+         the angular viscosity of the spinneret.
 
 All data are in SI units.
 
@@ -25,13 +23,14 @@ All data are in SI units.
 from deck import Deck
 from machine import RJSMachine
 from polymer import Polymer
-from models_rjs import *
+from modelling import RJSModel
 import numpy
 import matplotlib.pyplot as plt
 
 deck = Deck("deck.yaml")
 machine = RJSMachine(deck)
 polymer = Polymer(deck)
+model = RJSModel(polymer, machine)
 
 discretisation = int(deck.doc['Discretisation'])
 # The higher the discretisation number is, the finer the discretisation will be,
@@ -54,26 +53,25 @@ s0 = numpy.linspace(0.01, 0.1, discretisation)
 omega_th = []
 initial_velocity = []
 for l in range(discretisation):
-    omega_th.append(critical_rotational_velocity_threshold(surface_tension,
+    omega_th.append(model.critical_rotational_velocity_threshold(surface_tension,
                                                            orifice_radius, s0[l], rho))
-    initial_velocity.append(Initial_velocity(omega_th[l], s0[l]))
+    initial_velocity.append(model.Initial_velocity(omega_th[l], s0[l]))
 omega_th = numpy.array(omega_th)
 initial_velocity = numpy.array(initial_velocity)
 
-nu = kinematic_viscosity(mu, rho)
+nu = model.kinematic_viscosity(mu, rho)
 
-
-final_radius = []
+Final_radius = []
 for k in range(discretisation):
-    final_radius.append(final_radius_approx(orifice_radius, initial_velocity[k],
+    Final_radius.append(model.final_radius(orifice_radius, initial_velocity[k],
                                             nu, Rc, omega))
-final_radius = numpy.array(final_radius)
+Final_radius = numpy.array(Final_radius)
 
 fig = plt.figure()
 axes = fig.add_subplot(1, 1, 1)
 
 for i in range(discretisation):
-    axes.plot(s0[i], final_radius[i], 'ro')
+    axes.plot(s0[i], Final_radius[i], 'ro')
 axes.grid()
 axes.set_xlabel("Radius of the reservoir (m)", fontsize=16)
 axes.set_ylabel("Final radius (m)", fontsize=16)
@@ -83,9 +81,9 @@ axes.set_title(" %s / %s " % (name_machine, name_polymer), fontsize=16, y=1.)
 fig2 = plt.figure()
 axes = fig2.add_subplot(1, 1, 1)
 
-for i in range(discretisation-1):
-    if final_radius[i] <= 0.00002:
-        axes.plot(s0[i], final_radius[i], 'bo')
+for i in range(discretisation):
+    if Final_radius[i] <= 0.00002:
+        axes.plot(s0[i], Final_radius[i], 'bo')
 axes.grid()
 axes.set_xlabel("Radius of the reservoir (m)", fontsize=16)
 axes.set_ylabel("Final radius  (m)", fontsize=16)
