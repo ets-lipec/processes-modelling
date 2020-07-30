45415ad5f2bc4e097998726932c35dbd5363a8a5
AmelieLaurens <66320985+AmelieLaurens@users.noreply.github.com>
2020-07-30 18:09:07 +0200

    add a separate class to draw the graph and to compute the data
    - add a class Data to compute the data required to draw graphic
    - add a class Organization to organize the previous data in dictionnaries
    - add a class PointGraph to draw graphics
    -All these class replace the Graph class in figures.py in the master branch

---
+++ main.py
@@ -0,0 +1,22 @@
+from rotary_jet_spinning import *
+
+
+cwd = os.getcwd()
+
+deck = Deck(cwd + "/" + "deck.yaml")
+
+machine = RJSMachine(deck)
+
+polymer = Polymer(deck)
+
+model = RJSModel(polymer, machine)
+
+#graph = Graph(deck, polymer, machine, model)
+
+data = Data(deck, polymer, machine, model)
+
+organization = Organization(data, deck, machine, polymer, model)
+
+organized_data = organization.organize_data(data, deck, machine, polymer, model)
+
+graph = PointGraph(organized_data)
